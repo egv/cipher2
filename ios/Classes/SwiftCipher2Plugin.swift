31e09737a4bee2725e4455418213081456f68160
Gena E <genaevstratov@yandex-team.ru>
2019-10-31 15:46:08 +0300

    added new commands to ios part + refactored switch/case statement

--- ios/Classes/SwiftCipher2Plugin.swift
+++ ios/Classes/SwiftCipher2Plugin.swift
@@ -3,168 +3,199 @@ import UIKit
 import CryptoSwift
 
 extension String {
-  func fromBase64() -> String? {
-    guard let data = Data(base64Encoded: self, options: Data.Base64DecodingOptions(rawValue: 0)) else {
-        return nil
+    func fromBase64() -> String? {
+        guard let data = Data(base64Encoded: self, options: Data.Base64DecodingOptions(rawValue: 0)) else {
+            return nil
+        }
+        return String(data: data as Data, encoding: String.Encoding.utf8)
     }
-    return String(data: data as Data, encoding: String.Encoding.utf8)
-  }
 
-  func toBase64() -> String? {
-    guard let data = self.data(using: String.Encoding.utf8) else {
-        return nil
+    func toBase64() -> String? {
+        guard let data = self.data(using: String.Encoding.utf8) else {
+            return nil
+        }
+        return data.base64EncodedString(options: Data.Base64EncodingOptions(rawValue: 0))
     }
-    return data.base64EncodedString(options: Data.Base64EncodingOptions(rawValue: 0))
-  }
 }
 
 public class SwiftCipher2Plugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "cipher2", binaryMessenger: registrar.messenger())
-    let instance = SwiftCipher2Plugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "Encrypt_AesCbc128Padding7":
-      guard let args = call.arguments as? [String: String] else {
-        result(
-          FlutterError(
-            code: "ERROR_INVALID_PARAMETER_TYPE",
-            message: "the parameters data, key and iv must be all strings",
-            details: nil
-          )
-        )
-        return
-      }
-
-      if(args["data"] == nil || args["key"] == nil || args["iv"] == nil){
-        result(
-          FlutterError(
-            code: "ERROR_INVALID_PARAMETER_TYPE",
-            message: "the parameters data, key and iv must be all strings",
-            details: nil
-          )
-        )
-        return
-      }
-
-      let data = args["data"]!
-      let key = args["key"]!
-      let iv = args["iv"]!
-
-      let dataArray = Array(data.utf8)
-      let keyArray = Array(key.utf8)
-      let ivArray = Array(iv.utf8)
-
-      if(key.count != 16 || iv.count != 16){
-        result(
-          FlutterError(
-            code: "ERROR_INVALID_KEY_OR_IV_LENGTH",
-            message: "the length of key and iv must be all 128 bits",
-            details: nil
-          )
-        )
-        return
-      }
-
-      var encryptedBase64 = "";
-
-      do {
-
-        let encrypted = try AES(
-          key: keyArray, 
-          blockMode: CBC(iv: ivArray), 
-          padding: .pkcs7
-        ).encrypt(dataArray)
-        
-        let encryptedNSData = NSData(bytes: encrypted, length: encrypted.count)
-
-        encryptedBase64 = encryptedNSData.base64EncodedString(options:[])
-
-      } catch {
-        
-      }
-
-      result(encryptedBase64)
-
-    case "Decrypt_AesCbc128Padding7":
-
-      guard let args = call.arguments as? [String: String] else {
-        result(
-          FlutterError(
-            code: "ERROR_INVALID_PARAMETER_TYPE",
-            message: "the parameters data, key and iv must be all strings",
-            details: nil
-          )
-        )
-        return
-      }
-
-      if(args["data"] == nil || args["key"] == nil || args["iv"] == nil){
-        result(
-          FlutterError(
-            code: "ERROR_INVALID_PARAMETER_TYPE",
-            message: "the parameters data, key and iv must be all strings",
-            details: nil
-          )
-        )
-        return
-      }
-
-      let data = args["data"]!
-      let key = args["key"]!
-      let iv = args["iv"]!
-
-      //let dataArray = Array(data.utf8)
-      let keyArray = Array(key.utf8)
-      let ivArray = Array(iv.utf8)
-
-      if(key.count != 16 || iv.count != 16){
-        result(
-          FlutterError(
-            code: "ERROR_INVALID_KEY_OR_IV_LENGTH",
-            message: "the length of key and iv must be all 128 bits",
-            details: nil
-          )
-        )
-        return
-      }
-
-      //解码得到Array<Int32>
-      let encryptedData = NSData(base64Encoded: data, options:[]) ?? nil
-
-      if(encryptedData == nil || encryptedData!.length % 4 != 0){
-        result(
-          FlutterError(
-            code: "ERROR_INVALID_ENCRYPTED_DATA",
-            message: "the data should be a valid base64 string with length at multiple of 128 bits",
-            details: nil
-          )
-        )
-        return
-      }
-
-      let encrypted = [UInt8](encryptedData! as Data)
-
-      var plaintext = "";
-
-      do {
-        let decryptedData = try AES(
-          key: keyArray, 
-          blockMode: CBC(iv: ivArray), 
-          padding: .pkcs7
-        ).decrypt(encrypted)
-        
-        plaintext = String(bytes: decryptedData, encoding: String.Encoding.utf8)!
-      } catch {
-        
-      }
-
-      result(plaintext)
-
-    default: result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "cipher2", binaryMessenger: registrar.messenger())
+        let instance = SwiftCipher2Plugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+
+    private func encryptAesCbc128Padding7(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments as? [String: String] else {
+            result(
+                FlutterError(
+                    code: "ERROR_INVALID_PARAMETER_TYPE",
+                    message: "the parameters data, key and iv must be all strings",
+                    details: nil
+                )
+            )
+            return
+        }
+
+        if(args["data"] == nil || args["key"] == nil || args["iv"] == nil){
+            result(
+                FlutterError(
+                    code: "ERROR_INVALID_PARAMETER_TYPE",
+                    message: "the parameters data, key and iv must be all strings",
+                    details: nil
+                )
+            )
+            return
+        }
+
+        let data = args["data"]!
+        let key = args["key"]!
+        let iv = args["iv"]!
+
+        let dataArray = Array(data.utf8)
+        let keyArray = Array(key.utf8)
+        let ivArray = Array(iv.utf8)
+
+        if(key.count != 16 || iv.count != 16){
+            result(
+                FlutterError(
+                    code: "ERROR_INVALID_KEY_OR_IV_LENGTH",
+                    message: "the length of key and iv must be all 128 bits",
+                    details: nil
+                )
+            )
+            return
+        }
+
+        var encryptedBase64 = "";
+
+        do {
+
+            let encrypted = try AES(
+                key: keyArray,
+                blockMode: CBC(iv: ivArray),
+                padding: .pkcs7
+            ).encrypt(dataArray)
+
+            let encryptedNSData = NSData(bytes: encrypted, length: encrypted.count)
+
+            encryptedBase64 = encryptedNSData.base64EncodedString(options:[])
+
+        } catch {
+
+        }
+
+        result(encryptedBase64)
+
+    }
+
+    private func decryptAesCbc128Padding7(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments as? [String: String] else {
+            result(
+                FlutterError(
+                    code: "ERROR_INVALID_PARAMETER_TYPE",
+                    message: "the parameters data, key and iv must be all strings",
+                    details: nil
+                )
+            )
+            return
+        }
+
+        if(args["data"] == nil || args["key"] == nil || args["iv"] == nil){
+            result(
+                FlutterError(
+                    code: "ERROR_INVALID_PARAMETER_TYPE",
+                    message: "the parameters data, key and iv must be all strings",
+                    details: nil
+                )
+            )
+            return
+        }
+
+        let data = args["data"]!
+        let key = args["key"]!
+        let iv = args["iv"]!
+
+        //let dataArray = Array(data.utf8)
+        let keyArray = Array(key.utf8)
+        let ivArray = Array(iv.utf8)
+
+        if(key.count != 16 || iv.count != 16){
+            result(
+                FlutterError(
+                    code: "ERROR_INVALID_KEY_OR_IV_LENGTH",
+                    message: "the length of key and iv must be all 128 bits",
+                    details: nil
+                )
+            )
+            return
+        }
+
+        //解码得到Array<Int32>
+        let encryptedData = NSData(base64Encoded: data, options:[]) ?? nil
+
+        if(encryptedData == nil || encryptedData!.length % 4 != 0){
+            result(
+                FlutterError(
+                    code: "ERROR_INVALID_ENCRYPTED_DATA",
+                    message: "the data should be a valid base64 string with length at multiple of 128 bits",
+                    details: nil
+                )
+            )
+            return
+        }
+
+        let encrypted = [UInt8](encryptedData! as Data)
+
+        var plaintext = "";
+
+        do {
+            let decryptedData = try AES(
+                key: keyArray,
+                blockMode: CBC(iv: ivArray),
+                padding: .pkcs7
+            ).decrypt(encrypted)
+
+            plaintext = String(bytes: decryptedData, encoding: String.Encoding.utf8)!
+        } catch {
+
+        }
+
+        result(plaintext)
+    }
+
+    private func encryptAesGcm128(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+
+    }
+
+    private func decryptAesGcm128(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+
+    }
+
+    private func generateNonce(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "Encrypt_AesCbc128Padding7":
+            encryptAesCbc128Padding7(call, result: result)
+
+        case "Decrypt_AesCbc128Padding7":
+            decryptAesCbc128Padding7(call, result: result)
+
+        case "Encrypt_AesGcm128":
+            encryptAesGcm128(call, result: result)
+
+        case "Dencrypt_AesGcm128":
+            decryptAesGcm128(call, result: result)
+
+        case "Generate_Nonce":
+            generateNonce(call, result: result)
+            
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
 }
--- ios/cipher2.podspec
+++ ios/cipher2.podspec
@@ -3,7 +3,7 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'cipher2'
-  s.version          = '0.0.1'
+  s.version          = '0.2.0'
   s.summary          = 'A new flutter plugin project.'
   s.description      = <<-DESC
 A new flutter plugin project.
@@ -12,10 +12,12 @@ A new flutter plugin project.
   s.license          = { :file => '../LICENSE' }
   s.author           = { 'Your Company' => 'email@example.com' }
   s.source           = { :path => '.' }
+  
   s.source_files = 'Classes/**/*'
   s.public_header_files = 'Classes/**/*.h'
+  
   s.dependency 'Flutter'
-  s.dependency 'CryptoSwift', '~> 0.13.1'
+  s.dependency 'CryptoSwift', '~> 1.1.3'
   
   s.ios.deployment_target = '8.0'
 end
